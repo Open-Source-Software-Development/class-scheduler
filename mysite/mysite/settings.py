63a12305aae115cb364c86cb49069d3f9f24ee54
Patrick Coffman <patrick.coffman@mymail.champlain.edu>
2018-02-16 18:18:48 -0500

    Add database stubs
    We've got models for everything we decided we'd need - courses,
    professors, blocks, rooms, and room types; preferences and constraints;
    pregen and computed sections, and output hunks. Examples have been added
    for most of these.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,13 +32,13 @@ LOGIN_REDIRECT_URL = 'home'
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scheduler',
 ]
 
 MIDDLEWARE = [
---
+++ mysite/scheduler/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from scheduler.models import *
+
+admin.site.register(Division)
+admin.site.register(RoomType)
+admin.site.register(Professor)
+admin.site.register(Course)
+admin.site.register(Room)
+admin.site.register(Block)
+admin.site.register(PregenSection)
+admin.site.register(UserPreference)
+admin.site.register(UserConstaint)
+admin.site.register(Section)
+admin.site.register(Hunk)
