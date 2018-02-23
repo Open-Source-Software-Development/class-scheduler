9420f7230bee91c3dd61ed1f0501e894f434a5e7
tmatthews <tynan.matthews@mymail.champlain.edu>
2018-02-23 12:39:07 -0500

    add index and professor settings url paths

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -9,6 +9,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', polls.views.loginUser, name='login'),
 	url(r'^signup/', polls.views.signup, name='signup'),
+	url(r'^professor_settings/', polls.views.professor_settings, name='professor_settings'),
+	url(r'^index/', polls.views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 
+def professor_settings(request):
+	return render(request, 'profSettings.html')
+	
+def index(request):
+	return render(request, 'index.html')
+
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
@@ -12,15 +18,17 @@ def signup(request):
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 login(request, user)
-                return render(request, 'pages/Login.html')
+                return render(request, 'Login.html')
         else:
             match = "password didn't match"
             content = {'error': match}
-            return render(request, 'signup.html',content )
+            return render(request, 'signup.html', content)
     else:
         return render(request, 'signup.html')
 		
-		
+def logout_view(request):
+    logout(request, user)
+    return render(request, 'Login.html')		
 
 def loginUser(request):
     if request.method == 'POST':
@@ -33,11 +41,6 @@ def loginUser(request):
         else:
             failed = "Unable to login in"
             content = {'error':failed}
-            return render(request, 'pages/Login.html', content)
+            return render(request, 'Login.html', content)
     else:
-        return render(request, 'pages/Login.html')
-		
-
-def logout_view(request):
-    logout(request)
-    return render(request, 'pages/Login.html')
+        return render(request, 'Login.html')
