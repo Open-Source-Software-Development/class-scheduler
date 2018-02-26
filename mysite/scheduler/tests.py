3e572d74a9bbebc78e58a83664db4002e263e70a
Mordoug <morgan.seielstad@mymail.champlain.edu>
2018-02-26 14:01:41 -0500

    add admin.py

--- mysite/scheduler/tests.py
+++ mysite/scheduler/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+from .models import Entry
+
+from scheduler.models import Named
 # Create your tests here.
+
+class testModels():
+
+    def test_model_string_representaion(self):
+        entry = Entry(title="My entry title")
+        self.assertEqual(str(entry), entry.title)
+        
+    
