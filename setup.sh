dcf51e283d9b0b86dbfff48e263369688f74b80e
Patrick Coffman <patrick.coffman@mymail.champlain.edu>
2018-03-02 17:35:04 -0500

    Create start_server.sh

--- setup.sh
+++
@@ -1,4 +0,0 @@
-cd mysite
-py manage.py makemigrations
-py manage.py migrate
-py manage.py runserver
---
+++ start_server.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function exists {
+    if (which "$1" >/dev/null 2>&1); then
+        EXE="$1"
+	return 0
+    fi
+    return 1
+}
+
+function fail {
+	echo "ERROR: Could not find Python executable!"
+	exit 1
+}
+
+exists "python3" || exists "py3" || exists "python" || exists "py" || fail
+
+echo "found python executable: $EXE"
+
+cd mysite
+
+echo "setting up database..."
+$EXE manage.py makemigrations
+$EXE manage.py migrate
+
+echo "launching server..."
+$EXE manage.py runserver
