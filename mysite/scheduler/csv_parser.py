95e43e771ad60ef99d7526d7f48d6fcffffc69e5
Patrick Coffman <patrick.coffman@mymail.champlain.edu>
2018-03-05 10:41:20 -0500

    Ready CSV parser for database integration

--- mysite/scheduler/csv_parser.py
+++ mysite/scheduler/csv_parser.py
@@ -1,42 +1,83 @@
 import csv
 import models
 
-def create_models_for_type(filename, parse_mode):
+
+def parse_csv(filename):
     with open(filename) as f:
         reader = csv.reader(f)
-        header = None
         file_format = None
+        header = None
+        parse_mode = None
         for row in reader:
             if file_format is None:
                 file_format = row
             elif header is None:
                 header = row
-                parse_mode.set_header(header)
+                parse_mode = ParseMode.for_header(header)
             else:
                 model = parse_mode.add_row(row)
     return parse_mode.get_models()
 
+
 class ParseMode:
 
-    def set_header(self, header):
-        pass
+    modes = []
+
+    @classmethod
+    def register(cls, mode):
+        cls.modes.append(mode)
+        return mode
+
+    def __init__(self, header):
+        data = {}
+        for k in range(len(header)):
+            v = header[k]
+            data[v] = k
+        self.header = data
+
+    @classmethod
+    def can_parse(cls, header):
+        for i in cls.columns:
+            if i not in header:
+                return False
+        return True
+
+    @classmethod
+    def for_header(cls, header):
+        for mode in cls.modes:
+            if mode.can_parse(header):
+                return mode(header)
+        raise IndexError("couldn't determine what type of CSV this is - check the header")
 
     def add_row(self, row):
-        raise NotImplementedError()
+        return self.add_row_impl(self._parse_row(row))
 
     def get_models(self):
         raise NotImplementedError()
 
+    def add_row_impl(self, row):
+        raise NotImplementedError()
+
+    def _parse_row(self, row):
+        parsed = {}
+        for k, v in self.header.items():
+            parsed[k] = row[v] if v < len(row) else ""
+        return parsed
+
+@ParseMode.register
 class BlockParser(ParseMode):
 
-    def __init__(self):
+    columns = ("day", "start", "end")
+
+    def __init__(self, header):
+        parse_mode.set_header(header)
         self.block_count = 0
 
-    def add_row(self, row):
+    def add_row_impl(self, row):
         self.block_count += 1
 
     def get_models(self):
-        blocks = {}
+        blocks = []
         # Count backwards so we can set up the next_block associations
         old_block_b = None
         old_block_a = None
@@ -44,31 +85,37 @@ class BlockParser(ParseMode):
             block_b = models.Block(name=("{}B".format(i)), next_block=old_block_b, paired_with=None)
             block_a = models.Block(name=("{}A".format(i)), next_block=old_block_a, pared_with=block_b)
             old_block_b = block_b
-            old_alock_a = alock_a
-        return blocks.values()
+            old_block_a = block_a
+            blocks.append(block_b)
+            blocks.append(block_a)
+        return {models.Block: blocks}
 
+@ParseMode.register
 class RoomParser(ParseMode):
 
-    def __init__(self):
+    columns = ("building", "room", "type", "division")
+
+    def __init__(self, header):
+        super().__init__(header)
         self.rooms = []
         self.room_types = {}
 
-    def add_row(self, row):
-        name = "{}-{}".format(row["building"], row["capacity"])
+    def add_row_impl(self, row):
+        name = "{}-{}".format(row["building"], row["room"])
         room_type = self._get_room_type(row["type"])
         division = self._get_division(row["division"])
         model = models.Room(name=name, room_type=room_type, division=division)
-        self.rooms.add(model)
+        self.rooms.append(model)
 
     def get_models(self):
-        return list(self.rooms) + list(self.room_types.values())
+        return {models.Room: list(self.rooms), models.RoomType: list(self.room_types.values())}
 
     def _get_room_type(self, name):
         if name in self.room_types:
             return self.room_types[name]
         try:
-            return RoomType.objects.get(name=name)
-        except RoomType.DoesNotExist:
+            return models.RoomType.objects.get(name=name)
+        except models.RoomType.DoesNotExist:
             pass
         room_type = models.RoomType(name=name)
         self.room_types[name] = room_type
@@ -76,6 +123,6 @@ class RoomParser(ParseMode):
 
     def _get_division(self, name):
         try:
-            return Division.objects.get(name=name)
-        except Division.DoesNotExist:
+            return models.Division.objects.get(name=name)
+        except models.Division.DoesNotExist:
             return None
