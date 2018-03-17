a3a7301e71b0d0d29ccaf2017e17694f6943f7b1
Patrick Coffman <patrick.coffman@mymail.champlain.edu>
2018-03-17 14:34:56 -0400

    Add argument parser

---
+++ java/src/main/java/osd/main/FlagModule.java
@@ -0,0 +1,21 @@
+package osd.main;
+
+import dagger.Module;
+import dagger.Provides;
+import org.apache.commons.cli.ParseException;
+
+@Module
+public class FlagModule {
+
+    private final Flags flags;
+
+    public FlagModule(final String[] args) throws ParseException {
+        this.flags = new Flags(args);
+    }
+
+    @Provides
+    Flags providesFlags() {
+        return flags;
+    }
+
+}
