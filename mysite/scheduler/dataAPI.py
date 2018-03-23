42c74e8b400659bef779e7c91253bb32e54a39b2
Mordoug <morgan.seielstad@mymail.champlain.edu>
2018-03-23 12:45:45 -0400

    small changes to models.py, add dataAPI.py to act as a bidge to db data for front end.

---
+++ mysite/scheduler/dataAPI.py
@@ -0,0 +1,25 @@
+from polls.models import ProfessorConstraint, Professor, Block
+
+class DataAPI() :
+    """
+        Interact with the database here:
+        
+        Functionality:
+            
+    
+    """
+    
+    def get_blocks():
+        """
+            returns the block table
+        """
+        return Block.objects.all()
+        
+    def insert_professor_avalible(prof_name, timeblock):
+        """
+            Insert professor constriants into the proper location. 
+        """
+        
+        prof = Professor.object.get(name=prof_name)
+        block = Block.object.get(name=timeblock)
+        constraint = ProfessorConstraint.objects.create(professor=prof, block=block)
