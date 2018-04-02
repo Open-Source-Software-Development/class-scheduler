5b6265f3e216d1cd2034d7873b132de97e3fdc19
Stephen Johnson <stephen.johnson@mymail.champlain.edu>
2018-04-02 14:59:51 -0400

    configure connection url for hibernate

--- java/src/main/java/osd/database/TestRunner.java
+++ java/src/main/java/osd/database/TestRunner.java
@@ -1,10 +1,49 @@
 package osd.database;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
 public class TestRunner {
 
+	private static SessionFactory factory;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			factory = new AnnotationConfiguration().configure().addAnnotatedClass(ProfessorRecord.class).buildSessionFactory();
+		}catch(Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+	         throw new ExceptionInInitializerError(ex); 
+		}
+		TestRunner runner = new TestRunner();
+		runner.listProf();
+		System.out.println("end");
 	}
-
+	public void listProf() {
+		Session session = factory.openSession();
+	    Transaction tx = null;
+	    System.out.println("Start");
+	    try {
+	    	tx = session.beginTransaction();
+	        List professors = session.createQuery("FROM scheduler_professor").list(); 
+	        for (Iterator iterator = professors.iterator(); iterator.hasNext();){
+	        	ProfessorRecord prof = (ProfessorRecord) iterator.next(); 
+	            System.out.print("First Name: " + prof.getFirstName()); 
+	            System.out.print("  Last Name: " + prof.getLastName()); 
+	            System.out.println("  Div: " + prof.getDivisionId()); 
+	        }
+	        tx.commit();
+	    } catch (HibernateException e) {
+	    	if (tx!=null) tx.rollback();
+	        e.printStackTrace(); 
+	    } finally {
+	    	session.close(); 
+	    }
+	}
 }
+ 
